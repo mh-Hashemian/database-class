a6615f2162d7fa7ddd13252a3631daf4e329d104
mh-hashemian <hashemian0617@gmail.com>
2023-10-10 23:10:04 +0330

    add 2nd practice ERD and sql files

---
+++ tamrin/s2/insert_message.sql
@@ -0,0 +1,2 @@
+INSERT INTO `messages`(`title`, `body`, `user_id`) 
+			   VALUES ('UI issue', 'there is a problem with UI!', 1);
---
+++ tamrin/s2/insert_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users`(`username`, `email`, `password`) 
+			VALUES ('hashemian', 'hashemian@gmail.com', "abcd1234");
